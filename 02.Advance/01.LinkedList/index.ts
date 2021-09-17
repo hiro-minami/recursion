a5511a38dc9cca2c87e33ba37d0e0445ea9c898b
Hiro_Minami <hiro.0907@icloud.com>
2021-09-17 09:20:11 +0900

    片方向リスト - 追加・削除・逆転

--- 02.Advance/01.LinkedList/index.ts
+++ 02.Advance/01.LinkedList/index.ts
@@ -1,58 +1,80 @@
-// number型のノード
 class NumNode {
-    public data: number;
+    public data: number | null;
     public next: NumNode | null;
     constructor(data: number) {
         this.data = data;
         this.next = null;
     }
+    public addNextNode(newNode: NumNode): void {
+        let temp = this.next;
+        this.next = newNode;
+        newNode.next = temp;
+    }
 }
 
-// 片方向連結リスト
 class SinglyLinkedList {
-    public head: NumNode | null;
-    constructor(arr?: number[]) {
+    public head: NumNode;
+    constructor(arr: number[] | null) {
         this.head = arr.length > 0 ? new NumNode(arr[0]) : new NumNode(null);
-
         let currentNode = this.head;
         for (let i = 1; i < arr.length; i++) {
             currentNode.next = new NumNode(arr[i]);
             currentNode = currentNode.next;
         }
     }
-    // 走査
-    public scanning(): void {
+    public at(index: number): NumNode {
         let currentNode = this.head;
-        return this.scanningHelper(currentNode);
+        return this.atHelper(index, currentNode);
     }
-    private scanningHelper(currentNode: NumNode): void {
-        if (currentNode === null) return;
-        console.log(currentNode.data);
-        return this.scanningHelper(currentNode.next);
+    private atHelper(index: number, node: NumNode): NumNode {
+        if (node === null) return null;
+        if (index <= 0) return node;
+        return this.atHelper(index - 1, node.next);
     }
-    // 与えられたインデックスまでリストを反復する
-    public at(index: number): NumNode | null {
-        return this.atHelper(index, this.head);
+    public printList(): void {
+        console.log(this.printListHelper(this.head, ""));
     }
-    private atHelper(index: number, currentNode: NumNode): NumNode | null {
-        if (currentNode === null) return null;
-        if (index <= 0) return currentNode;
-        return this.atHelper(index - 1, currentNode.next);
+    private printListHelper(iterator: NumNode, str: string): string {
+        if (iterator.next === null) return str + iterator.data;
+        return this.printListHelper(iterator.next, str + iterator.data + " ");
     }
-    public findNode(value: number): number | null {
-        return this.findNodeHelper(value, this.head, 0);
+    public preappend(newNode: NumNode): void {
+        newNode.next = this.head;
+        this.head = newNode;
     }
-    private findNodeHelper(value: number, currentNode: NumNode, count: number): number | null {
-        if (currentNode === null) return null;
-        if (currentNode.data === value) return count;
-        return this.findNodeHelper(value, currentNode.next, count + 1);
+    public append(newNode: NumNode): void {
+        let iterator = this.scanning(this.head);
+        iterator.next = newNode;
+    }
+    private scanning(iterator: NumNode): NumNode {
+        if (iterator.next === null) return iterator;
+        return this.scanning(iterator.next);
+    }
+    public popFront(): void {
+        this.head = this.head.next;
+    }
+    public deleteNode(index: number): void {
+        if (index === 0) return this.popFront();
+        let targetNode = this.at(index - 1);
+        if (targetNode === null) return null;
+        targetNode.next = targetNode.next.next;
+    }
+    public reverse(): void {
+        let reverse = this.head;
+        this.head = this.head.next;
+        reverse.next = null;
+        while (this.head !== null) {
+            let temp = this.head;
+            this.head = this.head.next;
+            temp.next = reverse;
+            reverse = temp;
+        }
+        this.head = reverse;
     }
 }
 
-const numList = new SinglyLinkedList([35, 23, 546, 67, 86, 234, 56, 767, 34, 1, 98, 78, 555]);
+let numList = new SinglyLinkedList([35, 23, 546, 67, 86, 234, 56, 767, 34, 1, 98, 78, 555]);
 
-console.log(numList.at(2).data);
-console.log(numList.at(12).data);
-console.log(numList.findNode(234));
-console.log(numList.findNode(67));
-console.log(numList.findNode(767));
+numList.printList();
+numList.reverse();
+numList.printList();
